3ea6ad96458812dc313050dacc2f53442e781c4b
Victor Cordero <victordcordero@gmail.com>
2020-10-19 19:50:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+["dog" => "pet","apple" => "food", "plane" => "transportation", "brother" => "family"]
 end
 
 def add_element_to_end_of_array(array, element)
