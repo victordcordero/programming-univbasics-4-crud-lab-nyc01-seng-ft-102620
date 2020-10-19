3243a37a9e8c938a8fd8fa827f392808cf88173d
Victor Cordero <victordcordero@gmail.com>
2020-10-19 20:06:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  []
-add_element_to_end_of_array.push("value")
+  value = ["array", "element"]
+value.push("length")
 end
 
 def add_element_to_start_of_array(array, element)
