f6b006cb788cdb07bdf41d580c51780126125ffa
Victor Cordero <victordcordero@gmail.com>
2020-10-19 20:38:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-retrieve_element_from_index[2]
+retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
